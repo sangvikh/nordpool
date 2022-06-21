aa06145bd0920a677c9199a13ed0f566cdce56b4
sangvikh <harald.sangvik@redrock.no>
2022-06-21 13:14:11 +0200

    Using jsonpath to read values from json data

--- exchangerate.py
+++ exchangerate.py
@@ -1,6 +1,7 @@
 from urllib.request import urlopen
 import datefinder
 from datetime import datetime
+import jsonpath_ng
 
 # import json
 import json
@@ -13,23 +14,38 @@ response = urlopen(url)
 
 # storing the JSON response
 # from url in data
-data_json = json.loads(response.read())
+json_data = json.loads(response.read())
 
 # print the json response
-#print(data_json)
+#print(json_data)
 
 # Latest EURNOK price
-eurnok = list(data_json["data"]["dataSets"][0]["series"]["0:0:0:0"]["observations"].values())[-1]
+eurnok = list(json_data["data"]["dataSets"][0]["series"]["0:0:0:0"]["observations"].values())[-1]
 # Convert to float
 eurnok = float(eurnok[0])
 print(eurnok)
 
 # Get date of latest entry
-date = data_json["data"]["structure"]["dimensions"]["observation"]
+def gettime(data, index):
+    data = data.split(",")
+    return data[index*4]
+
+# Read date from json strucure
+# Values are not working correctly, cannot access "start" time etc.
+date = json_data["data"]["structure"]["dimensions"]["observation"]
 date = date[-1]["values"]
 date = str(date)
 
+#date = date.split(",")
+#print(len(date))
+#print(date)
+
 # Use datefinder to find last date in string
 date = list(datefinder.find_dates(date))
-print(len)
 print(date[-1])
+print("\n\n\n")
+
+#print(json_data["data"]["structure"]["dimensions"]["observation"][0].values())
+jsonpath_expr = jsonpath_ng.parse('data.structure.dimensions.observation[0].values[0].start')
+val = [match.value for match in jsonpath_expr.find(json_data)]
+print(val)
